fdd0d5b0a608ed4cdf50c5dd0d729528775fac38
Amaya <amaia@live.co.uk>
2018-05-16 20:17:56 +0100

    v1.5

---
+++ script.js
@@ -0,0 +1,11 @@
+$('#myModal').modal(options)
+
+/* Set the width of the side navigation to 250px */
+function openNav() {
+    document.getElementById("mySidenav").style.width = "50%";
+}
+
+/* Set the width of the side navigation to 0 */
+function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+}
